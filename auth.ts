a19792ce79cc908fe21ecf4d3a8b1f49f83f3a69
Steven Yung <stevenyung@fastmail.com>
2025-03-10 12:32:24 +0900

    add spotify scope for fetching data

--- auth.ts
+++ auth.ts
@@ -2,5 +2,29 @@ import NextAuth from "next-auth";
 import Spotify from "next-auth/providers/spotify";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Spotify],
+  providers: [
+    Spotify({
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email%20user-top-read ",
+    }),
+  ],
+  callbacks: {
+    jwt({ token, account }) {
+      if (account && account.provider === "spotify") {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+      if (token.refreshToken) {
+        session.refreshToken = token.refreshToken;
+      }
+      return session;
+    },
+  },
 });
