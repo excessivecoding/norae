5f5e89916db302ef166996a8d250e4ed4467e36f
Steven Yung <stevenyung@fastmail.com>
2025-03-15 15:26:43 +0900

    add api gateway

--- app/translation.tsx
+++ app/translation.tsx
@@ -1,6 +1,10 @@
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
 import { generateObject } from "ai";
 import { z } from "zod";
+
+export const openai = createOpenAI({
+  baseURL: `https://gateway.ai.cloudflare.com/v1/8180e951c3c180724ceac2921a96b823/norae/openai`,
+});
 
 export type TranslationResult = z.infer<typeof translationResultSchema>;
 
