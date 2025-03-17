c134bf138518e80f69cf6a59affd2c784701d7b6
Steven Yung <stevenyung@fastmail.com>
2025-03-17 16:37:59 +0900

    move ai gateway to env

--- app/openai.ts
+++ app/openai.ts
@@ -1,5 +1,5 @@
 import { createOpenAI } from "@ai-sdk/openai";
 
 export const openai = createOpenAI({
-  baseURL: `https://gateway.ai.cloudflare.com/v1/8180e951c3c180724ceac2921a96b823/norae/openai`,
+  baseURL: process.env.OPENAI_BASE_URL,
 });
