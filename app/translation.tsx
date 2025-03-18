5370ebf8533890eb7caa5f01c9b61c62a0a8bee6
Steven Yung <stevenyung@fastmail.com>
2025-03-18 18:13:34 +0900

    change modal for 4o mini

--- app/translation.tsx
+++ app/translation.tsx
@@ -47,7 +47,7 @@ function userPrompt(language: string, text: string, context?: string) {
 
 export async function translateFromKorean(text: string) {
   const { object } = await generateObject({
-    model: openai("gpt-4-turbo"),
+    model: openai("gpt-4o-mini"),
     schema: translationResultSchema,
     temperature: 0,
     messages: [
