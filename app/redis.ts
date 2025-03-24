c45cd715e00a14d9ec603fa93a69fe69bcf01fad
Steven Yung <stevenyung@fastmail.com>
2025-03-24 17:50:24 +0900

    change rate limiting to 100 per day

--- app/redis.ts
+++ app/redis.ts
@@ -5,7 +5,7 @@ export const redis = Redis.fromEnv();
 
 export const rateLimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  limiter: Ratelimit.slidingWindow(100, "1 d"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
